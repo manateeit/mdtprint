58bb1945cfdad3d614a1c29bdba3780cb78c114a
Ellen <ellen051394@gmail.com>
2015-02-08 16:36:48 +0800

    Test render pdf

--- app/js/rasterize.js
+++ app/js/rasterize.js
@@ -15,14 +15,14 @@ page.paperSize = {
 page.zoomFactor = 0.5;
 
 page.viewportSize = {
-  width: 600,
-  height: 470
+  width: 576,
+  height: 384
 };
     
 page.open(system.args[1], function (status) {
     window.setTimeout(function () {
         var size = page.evaluate(function () {
-            return {width: width = 600, height : 470 };
+            return {width: width = 576, height : 384 };
         });
 
         page.paperSize = size;
