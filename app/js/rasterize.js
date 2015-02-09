4c9c23ceda6f2ffb30b9c01ad9dc96ca677cd72e
Ellen <ellen051394@gmail.com>
2015-02-09 18:13:20 +0800

    adjust height smaller - 10

--- app/js/rasterize.js
+++ app/js/rasterize.js
@@ -16,13 +16,13 @@ page.zoomFactor = 0.5;
 
 page.viewportSize = {
   width: 800,
-  height: 640
+  height: 630
 };
     
 page.open(system.args[1], function (status) {
     window.setTimeout(function () {
         var size = page.evaluate(function () {
-            return {width: width = 800, height : 640 };
+            return {width: width = 800, height : 630 };
         });
         page.paperSize = size;
 
