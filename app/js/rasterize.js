a875b686e97045ac2775fe73c00168a778f39e46
Ellen <ellen051394@gmail.com>
2015-02-11 22:28:04 +0800

    remove semicolon

--- app/js/rasterize.js
+++ app/js/rasterize.js
@@ -12,13 +12,13 @@ page.paperSize = {
     }
 }
 
-page.zoomFactor = 0.7;
+page.zoomFactor = 0.5;
 
 page.viewportSize = {
   width: 600,
-  height: 600;
+  height: 600
 };
-
+    
 page.open(system.args[1], function (status) {
     window.setTimeout(function () {
         var size = page.evaluate(function () {
