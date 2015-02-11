bc8091335f931aa5f7a899997552ec90951b9221
Ellen <ellen051394@gmail.com>
2015-02-11 21:37:58 +0800

    Change viewport and zoom factor

--- app/js/rasterize.js
+++ app/js/rasterize.js
@@ -12,12 +12,12 @@ page.paperSize = {
     }
 }
 
-page.zoomFactor = 0.5;
+page.zoomFactor = 0.7;
 
 page.open(system.args[1], function (status) {
     window.setTimeout(function () {
         var size = page.evaluate(function () {
-            return {width: width = 900, height : 850 };
+            return {width: width = 940, height : 820 };
         });
         page.paperSize = size;
 
